b7df227fa2067e0daa95d5dfb65108bc898f494b
Xenderrr <90962222+Xenderrr@users.noreply.github.com>
2023-10-15 18:46:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,30 @@ import {encoded, translations} from './data.js'
 console.log("Let's rock")
 console.log(encoded, translations)
 
+function decryptFields(encoded, translations) {
+    if (!Array.isArray(encoded)) {
+        throw new Error('Input must be an array of objects.');
+    }
+
+    return encoded.map(item => {
+        const decryptedItem = {};
+        for (const key in item) {
+            if (item.hasOwnProperty(key)) {
+                if (key.endsWith('id')) {
+                    const field = key.slice(0, -2); // Удаляем суффикс "id"
+                    if (translations[field]) {
+                        decryptedItem[field] = translations[field][item[key]] || item[key];
+                    } else {
+                        decryptedItem[field] = item[key];
+                    }
+                } else {
+                    decryptedItem[key] = item[key];
+                }
+            }
+        }
+        return decryptedItem;
+    });
+}
 
 
 // console.log(decoded)
